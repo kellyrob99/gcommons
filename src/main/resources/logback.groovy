0e0ae2a84e9c8b971e5ea975a9934153c8b288cf
Evgeny Goldin <evgenyg@gmail.com>
2010-12-28 01:06:22 +0200

    File appender removed from Logback

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,23 +1,24 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
-import ch.qos.logback.core.FileAppender
 import static ch.qos.logback.classic.Level.INFO
 import static ch.qos.logback.classic.Level.WARN
 
-/**
+ /**
  * http://logback.qos.ch/manual/groovy.html
  * http://logback.qos.ch/manual/layouts.html
  */
 
+/*
 appender( "FILE", FileAppender  ) {
     file   = "gcommons.log"
     append = true
     encoder( PatternLayoutEncoder ) { pattern = "[%date][%-5level] [%logger] - [%msg]%n" }
 }
+*/
 
 appender( "CONSOLE", ConsoleAppender ) {
     encoder( PatternLayoutEncoder ) { pattern = "[%date][%-5level] [%logger] - [%msg]%n" }
 }
 
-root( WARN, [ "CONSOLE", "FILE" ] )
-logger( "com.goldin", INFO, [ "CONSOLE", "FILE" ] )
+root( WARN, [ "CONSOLE" ] )
+logger( "com.goldin", INFO, [ "CONSOLE" ] )
