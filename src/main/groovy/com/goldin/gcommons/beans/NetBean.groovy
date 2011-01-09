0bd095d4485fc92f8fdde9d7eb0cc96af4994ded
Evgeny Goldin <evgenyg@gmail.com>
2011-01-09 18:47:44 +0200

    IDEA files regenerated

--- src/main/groovy/com/goldin/gcommons/beans/NetBean.groovy
+++ src/main/groovy/com/goldin/gcommons/beans/NetBean.groovy
@@ -1,5 +1,6 @@
 package com.goldin.gcommons.beans
 
+import org.apache.commons.net.ftp.FTPClient
 
 /**
  * Network-related helper methods.
@@ -11,5 +12,12 @@ class NetBean extends BaseBean
      * Verifier, set by Spring
      */
     VerifyBean verify
+
+
+    List<String> ftpList( String remotePath, String includePattern, String excludePattern )
+    {
+        FTPClient client = new FTPClient()
+        null
+    }
 
 }
