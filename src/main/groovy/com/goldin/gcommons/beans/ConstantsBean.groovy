afd8d97e142331125953c780e1319c5d3801610a
Evgeny Goldin <evgenyg@gmail.com>
2011-01-11 16:11:00 +0200

    ConstantsBean - "public"s added

--- src/main/groovy/com/goldin/gcommons/beans/ConstantsBean.groovy
+++ src/main/groovy/com/goldin/gcommons/beans/ConstantsBean.groovy
@@ -7,16 +7,16 @@ import java.util.regex.Pattern
  */
 class ConstantsBean extends BaseBean
 {
-    final String  CRLF             = System.getProperty( 'line.separator' )
-    final String  USER_HOME        = System.getProperty( 'user.home' )
-    final String  USER_DIR         = System.getProperty( 'user.dir' )
+    public final String  CRLF             = System.getProperty( 'line.separator' )
+    public final String  USER_HOME        = System.getProperty( 'user.home' )
+    public final String  USER_DIR         = System.getProperty( 'user.dir' )
 
-    final File    USER_DIR_FILE    = new File( USER_DIR )
-    final File    USER_HOME_FILE   = new File( USER_HOME )
+    public final File    USER_DIR_FILE    = new File( USER_DIR )
+    public final File    USER_HOME_FILE   = new File( USER_HOME )
 
-    final Pattern NETWORK_PATTERN  = ~/^(?i)(http|scp|ftp):(?:\/)+(.+):(.+)@(.+):(.+)$/
+    public final Pattern NETWORK_PATTERN  = ~/^(?i)(http|scp|ftp):(?:\/)+(.+):(.+)@(.+):(.+)$/
 
-    final int    MILLIS_IN_SECOND  = 1000 // Milliseconds in a second
-    final int    SECONDS_IN_MINUTE = 60   // Seconds in a minute
-    final int    MILLIS_IN_MINUTE  = MILLIS_IN_SECOND * SECONDS_IN_MINUTE // Milliseconds in a minute
+    public final int    MILLIS_IN_SECOND  = 1000 // Milliseconds in a second
+    public final int    SECONDS_IN_MINUTE = 60   // Seconds in a minute
+    public final int    MILLIS_IN_MINUTE  = MILLIS_IN_SECOND * SECONDS_IN_MINUTE // Milliseconds in a minute
 }
