f1d4dd6b544331723d012366e649493bddac4a5c
Evgeny Goldin <evgenyg@gmail.com>
2011-02-21 19:09:30 +0200

    gc-33: Return ConfigurableApplicationContext from GCommons instead of ApplicationContext + "ideaProject.javaVersion    = '1.6.0_24'"

--- src/main/groovy/com/goldin/gcommons/GCommons.groovy
+++ src/main/groovy/com/goldin/gcommons/GCommons.groovy
@@ -3,17 +3,18 @@ package com.goldin.gcommons
 import com.goldin.gcommons.util.MopHelper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.context.ApplicationContext
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 import com.goldin.gcommons.beans.*
+
 
 /**
  * "GCommons" entry points
  */
 class GCommons
 {
-    private static ApplicationContext CONTEXT
-    private static ApplicationContext newContext(){ new ClassPathXmlApplicationContext( '/gcommons-application-context.xml' )}
+    private static ConfigurableApplicationContext CONTEXT
+    private static ConfigurableApplicationContext newContext(){ new ClassPathXmlApplicationContext( '/gcommons-application-context.xml' )}
     private static Logger LOG
 
     /**
@@ -66,12 +67,12 @@ class GCommons
     }
 
 
-    static ApplicationContext context       ( boolean refresh = false ) { CONTEXT = ( refresh ? newContext() : CONTEXT ) }
-    static ConstantsBean      constants     ( boolean refresh = false ) { getBean( ConstantsBean, refresh ) }
-    static VerifyBean         verify        ( boolean refresh = false ) { getBean( VerifyBean,    refresh ) }
-    static GeneralBean        general       ( boolean refresh = false ) { getBean( GeneralBean,   refresh ) }
-    static FileBean           file          ( boolean refresh = false ) { getBean( FileBean,      refresh ) }
-    static IOBean             io            ( boolean refresh = false ) { getBean( IOBean,        refresh ) }
-    static NetBean            net           ( boolean refresh = false ) { getBean( NetBean,       refresh ) }
-    static GroovyBean         groovy        ( boolean refresh = false ) { getBean( GroovyBean,    refresh ) }
+    static ConfigurableApplicationContext context   ( boolean refresh = false ) { CONTEXT = ( refresh ? newContext() : CONTEXT ) }
+    static ConstantsBean                  constants ( boolean refresh = false ) { getBean( ConstantsBean, refresh ) }
+    static VerifyBean                     verify    ( boolean refresh = false ) { getBean( VerifyBean,    refresh ) }
+    static GeneralBean                    general   ( boolean refresh = false ) { getBean( GeneralBean,   refresh ) }
+    static FileBean                       file      ( boolean refresh = false ) { getBean( FileBean,      refresh ) }
+    static IOBean                         io        ( boolean refresh = false ) { getBean( IOBean,        refresh ) }
+    static NetBean                        net       ( boolean refresh = false ) { getBean( NetBean,       refresh ) }
+    static GroovyBean                     groovy    ( boolean refresh = false ) { getBean( GroovyBean,    refresh ) }
 }
